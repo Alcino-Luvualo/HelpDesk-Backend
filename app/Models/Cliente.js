8831446f5f3b16d11afc00c38478622293ed5c9d
alcino.luvualo <alcino.luvualo@ideiasdinamicas.com>
2026-02-28 07:57:01 +0100

    Add fotoUrl getter to Cliente model

--- app/Models/Cliente.js
+++ app/Models/Cliente.js
@@ -3,9 +3,27 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
+/** @type {import('@adonisjs/framework/src/Hash')} */
+const Hash = use('Hash')
+
 class Cliente extends Model {
+  static boot () {
+    super.boot()
+
+    this.addHook('beforeSave', async (clienteInstance) => {
+      if (clienteInstance.dirty.password) {
+        clienteInstance.password = await Hash.make(clienteInstance.password)
+      }
+    })
+  }
+
   chamados(){
-    return this.hasMany('App/Models/Cliente')
+    return this.hasMany('App/Models/Chamado')
+  }
+
+  getFotoUrl (fotoUrl) {
+    if (!fotoUrl) return null
+    return fotoUrl.startsWith('http') ? fotoUrl : `http://localhost:3333${fotoUrl}`
   }
 }
 
